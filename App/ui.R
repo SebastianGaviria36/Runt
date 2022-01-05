17231880f8702216130806eba418c447e8e729e0
Sebastián Gaviria Sánchez <82337893+SebastianGaviria36@users.noreply.github.com>
2022-01-04 19:12:23 -0500

    Starting the App

---
+++ App/ui.R
@@ -0,0 +1,42 @@
+library(shiny)
+Ui <- fluidPage(
+   titlePanel("Titulo provisional"),
+   sidebarLayout(
+       sidebarPanel(
+           selectInput(
+               "visinput",
+               "Elija el contenido que desea ver",
+               c("Introducción",
+               "Comparación 2012 - 2016",
+               "Predicciones primer semestre del 2018",
+               "Sobre los autores")
+           ),
+           conditionalPanel(
+               condition = "input.visinput == 'Comparación 2012 - 2016'",
+               dateInput(
+                   "datetrain",
+                   "Seleccione la fecha para la visualización semanal",
+                   value = "2012-01-01",
+                   min  = "2012-01-01",
+                   max  = "2016-12-31",
+                   format = "dd-mm-yyyy",
+                   language = "es"
+               )
+           ),
+           conditionalPanel(
+               condition = "input.visinput == 'Predicciones primer semestre del 2018'",
+               dateInput(
+                   "datetest",
+                   "Seleccione la fecha para la visualización semanal",
+                   value = "2018-01-01",
+                   min  = "2018-01-01",
+                   max  = "2018-06-31",
+                   format = "dd-mm-yyyy",
+                   language = "es"
+               )
+           )
+           ),
+       mainPanel()
+   )
+
+)
