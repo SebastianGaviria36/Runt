11b72e5222c63deda461db9f8249ab3a5c220a66
SimonPGM <sigaleanom@unal.edu.co>
2022-01-04 20:01:07 -0500

    Shiny app partially done

--- App/server.R
+++ App/server.R
@@ -1,3 +1,11 @@
+source("./Render/rendermodels.R", encoding = "UTF-8")
 SERVER <- function(input, output){
-    
+    output$trainplot <- plotly::renderPlotly({
+        ensayo()
+    })
+
+    output$testplot <- plotly::renderPlotly({
+        ensayo()
+    })
 }
+
--- App/ui.R
+++ App/ui.R
@@ -1,42 +1,61 @@
 library(shiny)
+
 Ui <- fluidPage(
-   titlePanel("Titulo provisional"),
-   sidebarLayout(
-       sidebarPanel(
-           selectInput(
-               "visinput",
-               "Elija el contenido que desea ver",
-               c("Introducción",
-               "Comparación 2012 - 2016",
-               "Predicciones primer semestre del 2018",
-               "Sobre los autores")
+    titlePanel("Titulo provisional"),
+    sidebarLayout(
+        sidebarPanel(
+            selectInput(
+                "visinput",
+                "Elija el contenido que desea ver",
+                c(
+                    "Introducción",
+                    "Comparación 2012 - 2016",
+                    "Predicciones primer semestre del 2018",
+                    "Sobre los autores"
+                )
+            ),
+            conditionalPanel(
+                condition = "input.visinput == 'Comparación 2012 - 2016'",
+                dateInput(
+                    "datetrain",
+                    "Seleccione la fecha para la visualización semanal",
+                    value = "2012-01-01",
+                    min  = "2012-01-01",
+                    max  = "2016-12-31",
+                    format = "dd-mm-yyyy",
+                    language = "es"
+                )
+            ),
+            conditionalPanel(
+                condition = "input.visinput == 'Predicciones primer semestre del 2018'",
+                dateInput(
+                    "datetest",
+                    "Seleccione la fecha para la visualización semanal",
+                    value = "2018-01-01",
+                    min  = "2018-01-01",
+                    max  = "2018-06-31",
+                    format = "dd-mm-yyyy",
+                    language = "es"
+                )
+            )
+        ),
+        mainPanel(
+            conditionalPanel(
+               condition = "input.visinput == 'Introducción'",
+               includeHTML("./www/intro.html")
            ),
            conditionalPanel(
                condition = "input.visinput == 'Comparación 2012 - 2016'",
-               dateInput(
-                   "datetrain",
-                   "Seleccione la fecha para la visualización semanal",
-                   value = "2012-01-01",
-                   min  = "2012-01-01",
-                   max  = "2016-12-31",
-                   format = "dd-mm-yyyy",
-                   language = "es"
-               )
+               plotly::plotlyOutput("trainplot")
            ),
            conditionalPanel(
                condition = "input.visinput == 'Predicciones primer semestre del 2018'",
-               dateInput(
-                   "datetest",
-                   "Seleccione la fecha para la visualización semanal",
-                   value = "2018-01-01",
-                   min  = "2018-01-01",
-                   max  = "2018-06-31",
-                   format = "dd-mm-yyyy",
-                   language = "es"
-               )
-           )
+               plotly::plotlyOutput("testplot")
            ),
-       mainPanel()
-   )
-
+           conditionalPanel(
+               condition = "input.visinput == 'Sobre los autores'",
+              includeHTML("./www/aboutus.html")
+           )
+        )
+    )
 )
