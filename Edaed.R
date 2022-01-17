38ec59c7ebcf612247d4073f97915ec8ddef3638
Juan Jose <jgaleanoa@unal.edu.co>
2022-01-17 18:04:33 -0500

    Comment Edaed.R script with dataset construction

--- Edaed.R
+++ Edaed.R
@@ -1,10 +1,20 @@
+# IMPORTANTE.
+# LAS SIGUIENTES LINEAS DE CÓDIGO FUERON EJECUTADAS PARA LA CREACIÓN DE LOS 
+# DATOS Y SE ENCUNETRAN COMENTADAS DEBIDO A QUE YA NO SON NECESARIAS PUES 
+# LOS DATOS FUERON GUARDADOS EN OBJETOS .Rds, POR LO TANTO, SOLO SE CARGA
+# LA BASE DE DATOS PARA LA CREACIÓN DE GRÁFICOS
+#############################################################################
+#############################################################################
 # library(tidyverse)
 # library(lubridate)
 # library(magrittr)
 # library(rjson)
 # 
+# LECTURA DE LOS DATOS ENTREGADOS
 # datos <- readxl::read_xlsx("registros_autos_entrenamiento.xlsx")
 # 
+
+# LECTURA DEL OBJETO JSON PARA LA CREACIÓN DE FESTIVOS
 # json <- fromJSON(file = "holidays.json")
 # fechas <- c()
 # 
@@ -14,6 +24,7 @@
 # }
 # fechas <- as.Date(fechas)
 # 
+# CONSTRUCCIÓN DE LAS VARIABLES CATEGÓRCIAS
 # datos %<>%
 #   mutate(
 #     Fecha = as.Date(Fecha),
@@ -24,11 +35,15 @@
 #     Festivo = factor(if_else(Fecha %in% fechas, "Si", "No"))
 #   )
 # 
+
+# AÑADIENDO VARIABLES NUMÉRICAS
+
+# PIB
 # datosPIB <- readxl::read_xlsx("DatosPIB.xlsx")
 # 
 # datos <- inner_join(datos, datosPIB, by = "Ano")
 # 
-# #Agregando TRM del dolar
+# #AGREGANDO TRM DEL DOLAR
 # 
 # dolar <- readxl::read_xlsx("1.1.1.TCM_Serie_historica_IQY.xlsx",
 #                            range = "A8:B11004") %>%
@@ -37,13 +52,13 @@
 # 
 # datos <- inner_join(datos, dolar, by = "Fecha")
 # 
-# #Agregando salario minimo
+# #AGREGANDO SALARIO MÍNIMO
 # 
 # minimo <- readxl::read_xlsx("1.1.1.SLR_Serie_historica_IQY.xlsx")
 # 
 # datos <- inner_join(datos, minimo, by = "Ano")
 # 
-# #Agregando IPC e inflacion mensual
+# #AGREGANDO IPC E INFLACIÓN MENSUAL
 # ipc <- readxl::read_xlsx("1.2.5.IPC_Serie_variaciones.xlsx",
 #                  range = "A13:E822") %>%
 #   mutate(Ano = as.character(Ano),
@@ -52,10 +67,11 @@
 #   select(Ano, Mes, IPC, Inflacion_mensual) %>%
 #   filter(between(Ano, 2012, 2017))
 # 
-# #NOTAS: Inflación mensual melita, el resto es pure trash
 # meses <- unique(datos$Mes)
 # ipc$Mes <- meses[ipc$Mes]
 # datos <- inner_join(datos, ipc, by = c("Ano" = "Ano", "Mes" = "Mes"))
+
+# GUARDANDO DATOS DEFINITIVOS
 # saveRDS(datos, "datos.Rds")
 
 ################################################################################
@@ -69,6 +85,7 @@ p1 <- ggplot(datos, aes(Dia, Unidades, fill = Dia)) +
   theme_bw() +
   scale_y_continuous(breaks = seq(0, 3e+05, 1e+05), labels = seq(0, 3, 1))
 
+#Comportamiento del día en cada uno de los años
 p2 <- ggplot(datos, aes(Dia, Unidades, fill = Dia)) +
   geom_col() +
   facet_wrap(~Ano) +
@@ -80,6 +97,7 @@ p3 <- ggplot(datos, aes(Semana, Unidades, color = factor(Ano))) +
   geom_col(position = position_dodge2())
 
 #Matriz de correlaciones (variables numericas)
+#Evaluar multicolinealidad
 vars_corr <- cor(datos[, c("Unidades", "PIBanual", "PIBpersona", "TRM",
                            "Salariomin", "IPC", "Inflacion_mensual")])
 p4 <- ggcorrplot::ggcorrplot(vars_corr, method = "circle", 
@@ -88,6 +106,8 @@ p4 <- ggcorrplot::ggcorrplot(vars_corr, method = "circle",
                              colors = c("red", "white", "blue"))
 
 #Diagramas de dispersión de la respuesta vs covariables
+#Buscando tipo de relaciones entre covariables numéricas con la respuesta
+#Ej: cuadrático, cúbico, exponencial, a tramos (por ejemplo, splines)
 s1 <- ggplot(datos, aes(PIBanual, Unidades)) +
   geom_point() +
   geom_smooth(formula = y ~ x, method = lm) +
